e228e0f52dee54a5a042321b5d79e7e8cd06f9af
harshdev26 <156781178+harshdev26@users.noreply.github.com>
2025-06-26 09:42:05 +0000

    .....

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,66 @@
+// switch (key) {
+//    case value:
+
+//         break;
+
+//    default:
+
+//         break;
+
+
+// }
+
+
+const month = "march"
+
+switch (month) {
+    
+       case "jan":
+        console.log("january");
+        break;
+
+    
+        case "feb":
+        console.log("feb");
+        break
+
+    
+        case "march":
+        console.log("mar");
+        break
+
+    
+        case "april":
+        console.log("april");
+        break
+    
+        case "may":
+        console.log("may");
+    
+        case "june":
+        console.log("may");
+    
+        case "july":
+        console.log("may");
+    
+        case "august":
+    
+        console.log("may");
+    
+        case "september":
+        console.log("sept");
+    
+        case "october":
+        console.log("oct");
+    
+        case "november":
+        console.log("nov");
+    
+        case "december":
+        console.log("dec");
+    
+    default:
+        console.log("default case match:");
+        break;
+    
+}

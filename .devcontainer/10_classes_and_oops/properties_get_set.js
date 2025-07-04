bffcc1cee17f43dc9e71385fd8295a89cfa89d7f
harshdev26 <156781178+harshdev26@users.noreply.github.com>
2025-07-04 13:37:39 +0000

    ........

---
+++ .devcontainer/10_classes_and_oops/properties_get_set.js
@@ -0,0 +1,31 @@
+function User(email, password){
+    this.email = email;
+    this.password = password 
+   
+    Object.defineProperty(this, "email", {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this.email = value
+         }
+    })
+ 
+
+}
+    Object.defineProperty(this, "password", {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this.password = value
+         }
+    })
+ 
+
+   
+
+    const chai  = new User("chai@chai.com", "chai")
+
+console.log(chai.email);
+
